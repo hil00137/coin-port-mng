58749a994d71dd1cb4534eb315c0f50ae4e9c5a1
hil00137 <hil00137@gmail.com>
2023-02-22 22:07:25 +0900

    first commit

---
+++ src/main/kotlin/com/mcedu/coinportmng/CoinPortMngApplication.kt
@@ -0,0 +1,11 @@
+package com.mcedu.coinportmng
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoinPortMngApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoinPortMngApplication>(*args)
+}
---
+++ src/test/kotlin/com/mcedu/coinportmng/CoinPortMngApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mcedu.coinportmng
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoinPortMngApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
