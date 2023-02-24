9489d98c5c7b991292e2db2e52ff78cc3dc61f93
hil00137 <hil00137@gmail.com>
2023-02-24 10:14:29 +0900

    테스트용 임시 Entity삭제

--- src/main/kotlin/com/mcedu/coinportmng/repository/TestRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.mcedu.coinportmng.repository
-
-import com.mcedu.coinportmng.entity.TempEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TestRepository: JpaRepository<TempEntity, Long> {
-}
