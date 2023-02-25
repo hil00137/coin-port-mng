b4c2913cf0511449876e1e4fca6f182f05458dae
hil00137 <hil00137@gmail.com>
2023-02-25 23:39:11 +0900

    mustache 전환, API 통신확인

--- src/main/kotlin/com/mcedu/coinportmng/controller/RestController.kt
+++ src/main/kotlin/com/mcedu/coinportmng/controller/RestController.kt
@@ -2,7 +2,10 @@ package com.mcedu.coinportmng.controller
 
 import com.mcedu.coinportmng.dto.AccessInfoDto
 import com.mcedu.coinportmng.service.RepositoryInfoService
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,8 +15,19 @@ class RestController(
     private val repositoryInfoService: RepositoryInfoService
 ) {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
     @GetMapping("/v1/upbit/access-info")
     fun getAccessInfos(): List<AccessInfoDto> {
         return repositoryInfoService.getRepositoryInfos()
     }
+
+    @PostMapping("/v1/upbit/access-info")
+    fun saveAccessInfo(@RequestBody request: Request) {
+        log.info("{}", request)
+    }
 }
+
+data class Request(
+    val accessKey: String,
+    val secretKey: String,
+)
