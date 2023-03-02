62f187a8b8d5d26c28e83790c2b55de57777e446
hil00137 <hil00137@gmail.com>
2023-03-02 22:26:57 +0900

    fetch join

--- src/main/kotlin/com/mcedu/coinportmng/repository/RebalanceMngRepository.kt
+++ src/main/kotlin/com/mcedu/coinportmng/repository/RebalanceMngRepository.kt
@@ -3,8 +3,11 @@ package com.mcedu.coinportmng.repository
 import com.mcedu.coinportmng.entity.AccessInfo
 import com.mcedu.coinportmng.entity.RebalanceMng
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface RebalanceMngRepository : JpaRepository<RebalanceMng, Long> {
     fun findRebalanceMngByAccessInfo(accessInfo: AccessInfo): RebalanceMng?
+
+    @Query("select r from RebalanceMng r join fetch r.accessInfo where r.active = :active")
     fun findAllByActive(active: Boolean = true): List<RebalanceMng>
 }
