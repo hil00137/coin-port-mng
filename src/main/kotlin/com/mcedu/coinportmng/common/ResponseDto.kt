633147a95c726d7f1f43f34379d9cdaa62021c9e
hil00137 <hil00137@gmail.com>
2023-03-21 11:12:31 +0900

    exception handler 추가

--- src/main/kotlin/com/mcedu/coinportmng/common/ResponseDto.kt
+++ src/main/kotlin/com/mcedu/coinportmng/common/ResponseDto.kt
@@ -2,11 +2,16 @@ package com.mcedu.coinportmng.common
 
 data class ResponseDto<T>(
     val data: T?,
+    val status: Int = 200,
     val message: String = ""
 ) {
     companion object {
         fun success(msg: String): ResponseDto<Nothing> {
             return ResponseDto(data = null, message = msg)
+        }
+
+        fun fail(message: String): ResponseDto<Nothing> {
+            return ResponseDto(data = null, message = message, status = 500)
         }
     }
 }
--- src/main/kotlin/com/mcedu/coinportmng/config/ResponseAdvice.kt
+++ src/main/kotlin/com/mcedu/coinportmng/config/ResponseAdvice.kt
@@ -1,18 +1,20 @@
 package com.mcedu.coinportmng.config
 
 import com.mcedu.coinportmng.common.ResponseDto
+import org.slf4j.LoggerFactory
 import org.springframework.core.MethodParameter
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
 
 @RestControllerAdvice
 class ResponseAdvice: ResponseBodyAdvice<Any> {
-
+    private val log = LoggerFactory.getLogger(this::class.java)
     private val unSupportedTypes = setOf(ResponseEntity::class.java, ResponseDto::class.java)
     override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>>): Boolean {
         return !unSupportedTypes.contains(returnType.parameterType)
@@ -27,5 +29,11 @@ class ResponseAdvice: ResponseBodyAdvice<Any> {
         response: ServerHttpResponse
     ): Any? {
         return ResponseDto(data = body)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun exceptionHandler(exception: Exception): ResponseDto<Nothing> {
+        log.info(exception.message)
+        return ResponseDto.fail(exception.message ?: "")
     }
 }
