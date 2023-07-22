6ff3ab3ff593e157ff075e8606d9675063d97926
hil00137 <hil00137@gmail.com>
2023-07-22 22:27:46 +0900

    로우볼 추가

--- src/main/kotlin/com/mcedu/coinportmng/type/UpbitIndex.kt
+++ src/main/kotlin/com/mcedu/coinportmng/type/UpbitIndex.kt
@@ -2,7 +2,8 @@ package com.mcedu.coinportmng.type
 
 enum class UpbitIndex(val desc: String, val indexCode : String) {
     UTTI("Upbit Top 10 - 시가총액가중방식", "IDX.UPBIT.UTTI"),
-    UTTI_EW("Upbit Top 10 - 동일가중방식", "IDX.UPBIT.UTTI_EW");
+    UTTI_EW("Upbit Top 10 - 동일가중방식", "IDX.UPBIT.UTTI_EW"),
+    UBSI002("로우볼 Top 5", "IDX.UPBIT.UBSI002");
 
     fun getTicker(): String {
         return "IDX.$name"
